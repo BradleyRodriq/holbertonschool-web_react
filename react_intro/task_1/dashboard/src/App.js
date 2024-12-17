f3b79ae553b8209be4d214bed097eb3d8da39bd8
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-12-17 12:25:17 -0400

    react intro

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import logo from './holberton-logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="App-header">
+        <img className="logo" src={logo} alt=""/>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright { getFullYear() } - { getFooterCopy(false) }</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
